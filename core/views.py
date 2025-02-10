8abb483e7bd395a6d469ca6799fe32572d06f434
VXHugo65 <139810024+VXHugo65@users.noreply.github.com>
2025-02-10 11:30:43 +0200

    Update views.py

--- core/views.py
+++ core/views.py
@@ -38,7 +38,7 @@ def imprimir_recibo_imagem(request, pedido_id):
 
     # Ajuste do tamanho da fonte e cálculo da altura
     try:
-        font = ImageFont.truetype(font_path, 17)
+        font = ImageFont.truetype(font_path, 25)
     except IOError:
         font = ImageFont.load_default(size=21)
 
