f0dfb21d546334199e8cb7240f6250b3132e0405
VXHugo65 <139810024+VXHugo65@users.noreply.github.com>
2025-08-15 17:48:57 +0200

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -269,7 +269,7 @@ class PedidoAdmin(ModelAdmin, ImportExportModelAdmin):
     list_display_links = ('cliente', 'id')
     list_editable = ('status', 'pago')
     list_filter = (
-        'status', 'pago', ("criado_em", RangeDateTimeFilter), 'metodo_pagamento', ("data_pagamento", RangeDateTimeFilter)
+        'status', 'pago', ("data_pagamento", RangeDateTimeFilter), 'metodo_pagamento', ("criado_em", RangeDateTimeFilter)
     )
     list_filter_submit = True
     fieldsets = (
@@ -377,6 +377,7 @@ class ReciboAdmin(ModelAdmin):
             raise ValueError("O usuário logado não está associado a nenhum funcionário.")
 
         super().save_model(request, obj, form, change)
+
 
 
 
